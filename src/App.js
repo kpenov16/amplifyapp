22416c81b16f5da694d070efefd5a1665893301d
Kaloyan Penov <s133967@student.dtu.dk>
2020-08-02 06:49:52 +0200

    - test the automatic pipline

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>Extended by: Kaloyan Penov</p>
+	<p>Commiting this line will test automatic redeploy :)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
